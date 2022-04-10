cc04083e7bd6b6f71d1766d99c139be814ab5678
AgeevDmitryMinsk <ageev.dmitry@outlook.com>
2022-04-10 19:58:30 +0300

    add Redux+Lodash.throttle+ localStorage by Dan Abramov

--- src/state/store.ts
+++ src/state/store.ts
@@ -1,8 +1,8 @@
 // объединяя reducer-ы с помощью combineReducers,
 // мы задаём структуру нашего единственного объекта-состояния
+import throttle from 'lodash.throttle' // yarn add lodash.throttle //yarn add @types/lodash.throttle
 import { combineReducers, createStore } from 'redux'
 import { counterValueReducer } from './counterValueReducer'
-//import { loadState, saveState } from '../utils/localStorage-utils'
 
 //https://medium.com/@jrcreencia/persisting-redux-state-to-local-storage-f81eb0b90e7e
 //https://www.youtube.com/watch?v=U8f01SM8Kq4
@@ -37,11 +37,14 @@ export type AppRootState = ReturnType<typeof rootReducer>
 // непосредственно создаём store по рекомендации Dan Abramov:
 export const store = createStore(rootReducer, loadState())
 
-store.subscribe(() => {
-    saveState({
-        counterState: store.getState().counterState,
-    })
-})
+//Дроссель throttle: вызывает функцию не чаще одного раза в 1000 миллисекунд.
+store.subscribe(
+    throttle(() => {
+        saveState({
+            counterState: store.getState().counterState,
+        })
+    }, 1000)
+)
 
 // а это, чтобы можно было в консоли браузера обращаться к store в любой момент
 // @ts-ignore
